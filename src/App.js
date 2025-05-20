d5069b40a3cee75c2606fad9e54034bd6c895479
Abduvokh1D <abduvokhideurope@gmail.com>
2025-05-20 08:50:03 +0500

    change width and height using useEffect

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState("green");
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+  useEffect(function () {
+    window.addEventListener("resize", handleResize);
+    console.log("Event Added");
+    return function () {
+      window.removeEventListener("resize", handleResize);
+      console.log("Event Removed");
+    };
+  }, []);
   useEffect(
     function () {
-      document.title = `Count ${count} ${color}`;
+      document.title = `Size ${width} x ${height}`;
     },
-    [count, color]
+    [width, height]
   );
-  function handleAdd() {
-    setCount((count) => count + 1);
-  }
-  function handleRemove() {
-    setCount((count) => count - 1);
-  }
-  function changeColor() {
-    setColor((color) => (color === "green" ? "red" : "green"));
-  }
   return (
     <div className="App">
-      <p style={{ color: color }}>Count: {count}</p>
-      <button onClick={handleAdd}>Add</button>
-      <button onClick={handleRemove}>Remove</button>
-      <button onClick={changeColor}>Change Color</button>
+      <p>Window Width: {width}px</p>
+      <p>Window Height: {height}px</p>
     </div>
   );
 }
